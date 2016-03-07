fc0f224310d273ee80f76227d18fe7dfa92c22d1
Jan <jan.somers@student.kdg.be>
2016-03-07 14:22:39 +0100

    Loginactivity

---
+++ app/src/main/java/be/kandoe_groepj/kandoeproject/kandoeproject/application/presenter/SessionAdapter.java
@@ -0,0 +1,24 @@
+package be.kandoe_groepj.kandoeproject.kandoeproject.application.presenter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * Created by Jan on 7/03/2016.
+ */
+public class SessionAdapter extends RecyclerView.Adapter<SessionAdapter.ViewHolder> {
+    @Override
+    public SessionAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(SessionAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
--- app/src/main/java/be/kandoe_groepj/kandoeproject/kandoeproject/application/view/SessionActivity.java
+++ app/src/main/java/be/kandoe_groepj/kandoeproject/kandoeproject/application/view/SessionActivity.java
@@ -1,7 +1,9 @@
 package be.kandoe_groepj.kandoeproject.kandoeproject.application.view;
 
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 
 import be.kandoe_groepj.kandoeproject.R;
@@ -13,6 +15,11 @@ public class SessionActivity extends AppCompatActivity {
     @Bind(R.id.toolbar)
     Toolbar toolbar;
 
+    @Bind(R.id.recyclerSessions)
+    RecyclerView recyclerView;
+
+    @Bind(R.id.swipeSession)
+    SwipeRefreshLayout swipeRefreshLayout;
 
 
     protected void onCreate(Bundle savedInstanceState) {
